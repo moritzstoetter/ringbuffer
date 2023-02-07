554d6e21a4502a8d9a3778bb71c951d59c0f5d88
mo <moritz.stoetter@gmail.com>
2023-02-07 09:15:10 +0100

    counter book keeping

--- ringbuffer.hpp
+++ ringbuffer.hpp
@@ -6,19 +6,19 @@ template <typename T, size_t Size>
 class Ringbuffer
 {
 public:
-    bool empty() const 
+    bool empty() const
     {
-        return _empty;
+        return _count == 0;
     }
 
     bool full() const
     {
-        return _full;
+        return _count == Size;
     }
 
-    int count() const
-    {
-        return full() ? size() : std::distance(_readHead, _writeHead);
+    unsigned int count() const
+    {   
+        return _count;
     }
 
     int size() const
@@ -26,92 +26,102 @@ public:
         return _buffer.size();
     }
 
+    /*
+     * single value write
+     */
     void write(T &&val)
     {
         std::lock_guard<std::mutex> lock(_mutex);
 
-        if (_full)
+        if (full())
         {
             std::cout << "Full. \n";
             throw std::runtime_error("Ringbuffer is full!");
         }
 
         *_writeHead = val;
-        if (++(_writeHead) == _buffer.end())
+        if (++_writeHead == _buffer.end())
         {
             _writeHead = _buffer.begin();
         }
 
-        _empty = false;
-        _full = (_writeHead == _readHead);
+        ++_count;
     }
 
-    template<size_t N>
-    void write(std::span<T, N> &&vals) {
-        for (T val : vals) {
+    /*
+     * bulk write 
+     */
+    template <size_t N>
+    void write(std::span<T, N> &&vals)
+    {
+        for (T val : vals)
+        {
             this->write(std::move(val));
         }
     }
 
+    /* 
+     * single value read
+     */
     std::optional<T> read()
     {
         std::lock_guard<std::mutex> lock(_mutex);
 
-        if (_empty)
+        if (empty())
         {
             std::cout << "Empty! \n";
             return std::nullopt;
         }
 
         auto ret = std::optional<T>(*_readHead);
-        if (++(_readHead) == _buffer.cend())
+        if (++_readHead == _buffer.cend())
         {
             _readHead = _buffer.cbegin();
         }
 
-        _empty = (_writeHead == _readHead);
-        _full = false;
+        --_count;
 
         return ret;
     }
 
-    // template <size_t Num> 
-    // std::optional<std::span<T, Num>> read(Num) {
-    //     if (Num > count()) {
-    //         return std::nullopt;
-    //     }
+    /* 
+     * bulk value read 
+     */
+    std::span<T, std::dynamic_extent> readAll()
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
 
+        std::span ret{_buffer.data(), count()};
 
-    // }
+        _readHead = _writeHead;
+        _count = 0; 
 
-    T peek()
+        return ret;
+    }
+
+    T peek() const
     {
         return *_readHead;
     }
 
-    void print() const {
+    void print() const
+    {
+        std::cout << "Size: " << size() << ", Count: " << count();
         std::string one;
         std::string three;
-        for(int i = 0; i<Size; ++i) {
-            if (_buffer.cbegin() + i == _readHead) {
-                one = " r ->";
-            } else {
-                one = "";
-            }
-            if (_buffer.begin() + i == _writeHead) {
-                three = " <- w";
-            } else {
-                three = "";
-            }
-            printf("%5s %-5x %-5s\n", one.c_str(), *(_buffer.cbegin()+i), three.c_str() );
+        for (int i = 0; i < Size; ++i)
+        {
+            one = _buffer.cbegin() + i == _readHead ? " r ->" : "";
+            three = _buffer.begin() + i == _writeHead ? " <- w" : "";
+            printf("%5s %-5x %-5s\n", one.c_str(), *(_buffer.cbegin() + i), three.c_str());
         }
     }
 
 private:
     std::mutex _mutex;
     std::array<T, Size> _buffer;
+    unsigned int _count = 0;
     typename std::array<T, Size>::iterator _writeHead = _buffer.begin();
     typename std::array<T, Size>::const_iterator _readHead = _buffer.cbegin();
-    bool _empty = true; // !full && (_readHead == _writeHead)
-    bool _full = false;
+    
 };
